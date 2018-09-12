bf9b226dbf1c5730e32d97f20f4fcd8d72686020
Loreto Vaquero <loret.vaquero@gmail.com>
2018-09-12 09:59:35 +0200

    Add StylesDataBox.js and link with the component

--- src/UdaDataBox.js
+++ src/UdaDataBox.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import List from './List.js';
 import Coordinates from './Coordinates.js';
+import { DataBox } from '../stylesheets/DataBox';
+
 class UdaDataBox extends Component {
   render() {
     return (
-      <div className="App">
+      <div style={DataBox}>
         <List />
         <Coordinates />
       </div>
---
+++ src/stylesheets/StylesDataBox.js
@@ -0,0 +1,9 @@
+
+const DataBox = {
+    display: 'flex',
+    border: '1px' 'solid' 'black',
+    min-width:'430px',
+    max-width:'700px',
+    min-height:'90px',
+    max-height:'140px'
+}
